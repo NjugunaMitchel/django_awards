b1576bb3243733295302328af8b2a34438476cb3
mishel254 <sonimichie@gmail.com>
2020-12-02 10:12:02 +0300

    db models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=40)
+    link = models.CharField(max_length=255)
+    image = CloudinaryField('image',default="image.jpg")
+    
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    projects=models.ForeignKey(Project,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        self.username
+
+
+    
